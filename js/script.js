9d82fa77a8b2b97ee73e6236bcc374d088a90236
TeronKairos <S.Kuzmin2004@ya.ru>
2024-01-24 18:40:18 +0400

    cjv

--- js/script.js
+++ js/script.js
@@ -71,3 +71,73 @@ if (delayAdd) {
         document.getElementById("forAdd").innerHTML = numClicks
     })
 }
+
+function serializeForm(formNode) {
+    return new FormData(formNode)
+}
+
+async function sendData(data) {
+    return await fetch('/api/apply/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'multipart/form-data' },
+        body: data,
+    })
+}
+
+async function handleFormSubmit(event) {
+    event.preventDefault()
+    console.log('Отправка!')
+    const data = serializeForm(event.target)
+    const response = await sendData(data)
+}
+
+const applicantForm = document.getElementById('mars-once')
+applicantForm.addEventListener('submit', handleFormSubmit)
+
+function calkulation(form) {
+    let data = new FormData(form)
+    let result = 0;
+    switch (data.get('oper-logic')) {
+        case 'plus':
+            console.log('Плюс')
+            result = parseInt(data.get('el1')) + parseInt(data.get('el2'))
+            break;
+        case 'minus':
+            console.log('Минус')
+            result = parseInt(data.get('el1')) - parseInt(data.get('el2'))
+            break;
+
+        case 'mult':
+            console.log('Умножить')
+            result = parseInt(data.get('el1')) * parseInt(data.get('el2'))
+            break;
+
+        case 'part':
+            console.log('Разделить')
+            result = parseInt(data.get('el1')) / parseInt(data.get('el2'))
+            break;
+        case 'pow':
+            console.log('Возведение в степень')
+            result = parseInt(data.get('el1')) ** parseInt(data.get('el2'))
+            break
+
+
+        default:
+            console.log('Несработало')
+            break;
+    }
+    return result;
+
+}
+
+function handler(event) {
+    event.preventDefault()
+    let a = calkulation(event.target)
+    document.body.append(" " + a)
+    document.getElementById('rez').innerHTML = a
+
+}
+
+
+const useFun = document.getElementById('useFun')
+useFun.addEventListener('submit', handler)
